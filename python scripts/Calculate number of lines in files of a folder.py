f1917b171b915b0e3952e7d94102621d285c9295
Sayyid <78584556+sayyid5416@users.noreply.github.com>
2022-05-02 17:08:49 +0530

    Refactoring

--- python scripts/Calculate number of lines in files of a folder.py
+++ python scripts/Calculate number of lines in files of a folder.py
@@ -1,57 +1,47 @@
-# Imports
 import os
 
 
 
-def main_____fctn(current_dir, extension):
-    
-    directory_data = os.walk(current_dir)
-    final_num = 0
-    
-    for root, folders_list, files_list in directory_data:
-        
-        for f in files_list:
-            
-            my_file = os.path.join(root, f)
-            my_file_extension = my_file.split('.')[-1]
-            
-            if extension == my_file_extension:
-                try:
-                    with open(my_file) as target:
-                        length = len(target.readlines())
-                        final_num += length
-                        print(f'-----> {my_file} ==== {length}')
 
-                except UnicodeError:
-                    with open(my_file, encoding='utf-8') as target:
-                        length = len(target.readlines())
-                        final_num += length
-                        print(f'-----> {my_file} ==== {length}')
-                        
-                except Exception as e:
-                    print(f'[Error in {my_file}] {e}')
-                    
-    return final_num
+def get_lines(filePath:str):
+    """ Returns: Number of lines in a file on location `filePath` """
+    try:
+        with open(filePath) as target:
+            return len(target.readlines())
+
+    except UnicodeError:
+        with open(filePath, encoding='utf-8') as target:
+            return len(target.readlines())
+            
+    except Exception as e:
+        print(f'[Error in {filePath}] {e}')
+        return
+
+
+def get_total_lines(myDir, extension):
+    """ Returns: Total lines in all `files.extension` in the `myDir` """
+    totalLines = 0
+    for rootDir, foldersList, filesList in os.walk(myDir):
+        for f in filesList:
+            myFile = os.path.join(rootDir, f)
+            if myFile.endswith(extension):
+                lines = get_lines(myFile)
+                if lines is None: continue
+                totalLines += lines
+                print(f'-----> {myFile} ==== {lines}')   
+    return totalLines
 
 
 
 
 if __name__ == "__main__":
-    os.system(
-        'title Calculate Number of lines'
-    )                                                               # Title
+    os.system('title Calculate Number of lines')                                                                # Title
     while True:
         try:
-            dir_ = input('> Enter the directory location: (press enter for current dir)') or os.getcwd()      # Directory
-            file_ext = input('> Enter file extension: ')                                                      # File type
-            number_of_lines = main_____fctn(dir_, file_ext)                                                   # Main Function
-        except Exception as e:
-            print(f'[Main Error] {e}')
-        else:
-            print(
-                '\n\nTotal Number of lines in all '
-                f'".{file_ext}" files of "{dir_}" = {number_of_lines}'
-            )                                                                                   # Output
-        finally:
-            print('\n\n\n\n')
+            dir_ = input('> Enter the directory location: (press enter for current dir)') or os.getcwd()        # Directory
+            fileExt = input('> Enter file extension: ')                                                         # File type
+            linesNum = get_total_lines(dir_, fileExt)                                                           # Main Function
+        except Exception as e:  print(f'[Main Error] {e}')
+        else:                   print(f'\n\nTotal lines in all ".{fileExt}" files of "{dir_}" = {linesNum}')    # Output
+        finally:                print('\n\n\n\n')
 
